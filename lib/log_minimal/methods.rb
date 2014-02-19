d4a6949248ebc9fb45c12c1e52adfcd7a65206ed
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2014-02-19 16:38:16 +0900

    more minimal

--- lib/log_minimal/methods.rb
+++ lib/log_minimal/methods.rb
@@ -1,15 +1,9 @@
-require 'active_support/core_ext/time'
-
 module LogMinimal
   module Methods
     [:fatal, :error, :warn, :info, :debug].each do |method|
       define_method "#{method}f" do |message|
-        time = Time.now.iso8601
-        level = method.to_s.upcase
         caller = "%s#%s:%s" % [self.class, caller(1)[0].scan(/in `(.*)'/)[0][0], caller(1)[0].scan(/:(\d+):/)[0][0]]
-        msg = message
-
-        logger.send(method, "%s\t[%s]\t%s\t%s" % [time, level, caller, msg])
+        logger.send(method, "%s\t%s" % [caller, message])
       end
     end
   end
